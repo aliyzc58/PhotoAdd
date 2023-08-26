a0ed6501c42b4d4888ed247236ebd53b6a61594e
aliyazıcı <93381581+aliyzc58@users.noreply.github.com>
2023-08-26 09:42:25 +0300

    Add files via upload

---
+++ WEB/Program.cs
@@ -0,0 +1,32 @@
+using WEB.APIHandler;
+using WEB.FileUploads;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IApiHandler, ApiHandler>();
+builder.Services.AddScoped(typeof(IFileUpload), typeof(FileUpload));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
